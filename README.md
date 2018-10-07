b7b4e12d834af48de9d9c64208a65dd5e2e10721
rrsilaya <rrsilaya@gmail.com>
2018-10-08 00:51:22 +0800

    Change variables to args

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 To generate PDF, ensure that you have installed `imagemagick`. To install, go [here](https://www.imagemagick.org/script/download.php).
 
-### Instructions
+### Installation
+Just do a `yarn global add ggrab` or `npm i -g ggrab` and you're good to go!
+
+### Installation from Source
 1. Clone this repository.
 2. Install the dependencies with `yarn install`.
 3. Change script variables `URL` and `SLIDES` with the URL and number of slides respectively. As of now, the script can't detect the total number of slides.
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+#!/usr/bin/env node
 const puppeteer = require('puppeteer');
 const im = require('imagemagick');
 const fs = require('fs');
-
-const SLIDES = 73;
-const URL = 'https://docs.google.com/presentation/d/e/2PACX-1vSiTALGQReVaqVzgssiRu7Q8h--j1M7lrbFBiBoRJLmL0AyldAcdtKQR1lmXxaY1IdlFFMOxFfHu8KJ/pub?start=false&loop=false&delayms=3000';
-const DIRECTORY = 'screenshots';
+const { ArgumentParser } = require("argparse");
+const ora = require('ora');
+const rimraf = require('rimraf');
 
 const config = {
   args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  //headless: false,
 };
 
-if (!fs.existsSync(DIRECTORY)) fs.mkdirSync(DIRECTORY);
+// Parser configuration
+const parser = new ArgumentParser({
+  version: '0.0.1',
+  description: 'Grabs published Google Slides',
+});
+
+parser.addArgument('url', { help: 'URL of published Google Slides' });
+parser.addArgument(['-c', '--count'], { help: 'Number of slides', required: true });
+parser.addArgument(['-o', '--output'], { help: 'Output file', defaultValue: 'slides.pdf' });
+parser.addArgument(['--no-pdf'], { help: 'Do not generate PDF', action: 'storeTrue' });
+
+const args = parser.parseArgs();
+const [, filename] = args.output.match(/(.+?)(\.[^.]*$|$)/);
+
+if (!fs.existsSync(filename)) fs.mkdirSync(filename);
 
 (async () => {
   const browser = await puppeteer.launch(config);
@@ -20,33 +33,38 @@ if (!fs.existsSync(DIRECTORY)) fs.mkdirSync(DIRECTORY);
 
   await page.setViewport({ width: 1200, height: 800 });
 
-  console.log('Opening URL (This might take a while depending on your internet connection)');
-  await page.goto(URL, { waitUntil: 'networkidle2', timeout: 0 });
-  console.log('Successfully loaded URL');
+  let spinner = ora('Opening URL in background (This might take a while)').start()
+  await page.goto(args.url, { waitUntil: 'networkidle2', timeout: 0 });
+  spinner.succeed();
 
-  console.log('Starting slide grabbing');
+  spinner = ora('Starting slide grabbing').start();
   const el = await page.$('.punch-viewer-content');
   await page.waitFor(1500);
 
   let i = 1;
   do {
-    console.log(`Taking screenshot for slide ${i}`);
-    await el.screenshot({ path: `${DIRECTORY}/${i}.png` });
+    spinner.text = `Grabbing image of slide ${i}`;
+    await el.screenshot({ path: `${filename}/${i}.png` });
     await page.keyboard.press('ArrowRight');
     await page.waitFor(300);
 
     i++;
-  } while (i <= SLIDES);
+  } while (i <= parseFloat(args.count));
 
   await browser.close();
-  console.log('Successfully grabbed slides');
+  spinner.succeed('Successfully grabbed slides');
 
-  const files = Array.from({ length: SLIDES }).map((_, i) => `${DIRECTORY}/${i + 1}.png`);
-
-  console.log('Compiling images to PDF');
-  im.convert([...files, '-quality', '100', `${DIRECTORY}/slides.pdf`], (err, stdout) => {
-    if (err) console.log('Failure to generate PDF');
-    console.log(stdout);
-  });
-
+  if (!args['no-pdf']) {
+    const files = Array.from({ length: args.count }).map((_, i) => `${filename}/${i + 1}.png`);
+  
+    spinner = ora('Compiling images to PDF').start();
+    im.convert([...files, '-quality', '100', `${filename}.pdf`], (err, stdout) => {
+      if (err) {
+        spinner.fail("Failure to generate PDF");
+      } else {
+        spinner.succeed("Successfully generated PDF");
+        rimraf.sync(filename);
+      }
+    });
+  }
 })();
