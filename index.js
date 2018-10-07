bb034c523e78d8cf222d30e80066e88845886285
rrsilaya <rrsilaya@gmail.com>
2018-10-07 23:16:57 +0800

    Add generation of PDF

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
+const im = require('imagemagick');
+const fs = require('fs');
 
 const SLIDES = 73;
 const URL = 'https://docs.google.com/presentation/d/e/2PACX-1vSiTALGQReVaqVzgssiRu7Q8h--j1M7lrbFBiBoRJLmL0AyldAcdtKQR1lmXxaY1IdlFFMOxFfHu8KJ/pub?start=false&loop=false&delayms=3000';
+const DIRECTORY = 'screenshots';
 
 const config = {
   args: ['--no-sandbox', '--disable-setuid-sandbox'],
   //headless: false,
 };
+
+if (!fs.existsSync(DIRECTORY)) fs.mkdirSync(DIRECTORY);
 
 (async () => {
   const browser = await puppeteer.launch(config);
@@ -26,7 +31,7 @@ const config = {
   let i = 1;
   do {
     console.log(`Taking screenshot for slide ${i}`);
-    await el.screenshot({ path: `screenshots/${i}.png` });
+    await el.screenshot({ path: `${DIRECTORY}/${i}.png` });
     await page.keyboard.press('ArrowRight');
     await page.waitFor(300);
 
@@ -35,4 +40,13 @@ const config = {
 
   await browser.close();
   console.log('Successfully grabbed slides');
+
+  const files = Array.from({ length: SLIDES }).map((_, i) => `${DIRECTORY}/${i + 1}.png`);
+
+  console.log('Compiling images to PDF');
+  im.convert([...files, '-quality', '100', `${DIRECTORY}/slides.pdf`], (err, stdout) => {
+    if (err) console.log('Failure to generate PDF');
+    console.log(stdout);
+  });
+
 })();
